5868f4982b4d9462f96c0ee513480e0e7031dcb0
Alex Chan <alexkchan@gmail.com>
2015-02-22 21:52:46 -0800

    Hana4

---
+++ README.md
@@ -0,0 +1,4 @@
+# HAHA
+ABOUT US
+
+We Are a team of young individuals, we want to make a small difference by encouraging people to focus more on mindfulness. 
---
+++ client/templates/posts/posts_list.js
@@ -0,0 +1,43 @@
+Template.postsList.helpers({
+  postsWithRank: function() {
+    return this.posts.map(function(post, index, cursor) {
+      post._rank = index;
+      return post;
+    });
+  },
+
+  ownPost: function() {
+    return this.userId == Meteor.userId();
+  },
+
+  indexColor: function(){
+
+  	// latestPosts = Posts.find({}, {sort: {createdAt: -1}, limit: 2});
+
+  	postCount = Posts.find().count();
+
+  	console.log(postCount);
+
+  	switch(true)
+  	{
+  		case (postCount > 4):
+  			return "#7DBD7D";
+  			break;
+  		case (postCount > 2):
+  			return "#FFDE59";
+  			break;
+  		case (postCount > 0):
+  			return "#FFAD5C";
+  			break;
+  		default:
+  			return "#D14719";
+  			break;
+  	}
+
+  },
+
+  numPosts: function(){
+    return Posts.find().count();
+  }
+
+});
